cd38f3376cc75abacd5b6c8683b39e4167cce5b1
Amit Sarker <you@example.com>
2024-09-03 09:24:53 +0600

    password encryption using bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -48,5 +50,15 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+  }
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
