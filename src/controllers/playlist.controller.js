836e797f13d4b8316fc48f2e15ae9806633c6c81
Amit Sarker <you@example.com>
2024-09-24 23:54:50 +0600

    addVideoToPlayList controllers added

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Playlist } from "../models/playlist.model.js";
@@ -7,7 +7,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const createPlaylist = asyncHandler(async (req, res) => {
   //   console.log("Playlist is hitting");
   const { name, description, videoId } = req.body;
-  console.log(name, description, videoId);
+  //   console.log(name, description, videoId);
   if (!mongoose.isValidObjectId(videoId)) {
     throw new ApiError(200, "Invalid Video Id");
   }
@@ -25,5 +25,39 @@ const createPlaylist = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, playlist, "Video added successfully"));
 });
-
-export { createPlaylist };
+const addVideoToPlayList = asyncHandler(async (req, res) => {
+  const { videoId, playlistId } = req.params;
+  console.log(videoId, playlistId);
+  if (!isValidObjectId(videoId) && !isValidObjectId(playlistId)) {
+    throw new ApiError(401, "Invalid video or playlist ID");
+  }
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(401, "Playlist doesn't exist's");
+  }
+  if (playlist.videos.includes(videoId)) {
+    throw new ApiError(400, "Video is already in the playlist");
+  }
+  playlist.videos.push(videoId);
+  console.log(playlist);
+  const updatedPlaylist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $set: {
+        videos: playlist.videos,
+      },
+    },
+    { new: true }
+  );
+  console.log(updatedPlaylist);
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updatedPlaylist,
+        "Video added to playlist successfully"
+      )
+    );
+});
+export { createPlaylist, addVideoToPlayList };
--- src/routes/playlist.route.js
+++ src/routes/playlist.route.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import {
+  addVideoToPlayList,
+  createPlaylist,
+} from "../controllers/playlist.controller.js";
 
 const router = Router();
 router.use(verifyJWT);
 
 router.route("/").post(createPlaylist);
+router.route("/add/:videoId/:playlistId").patch(addVideoToPlayList);
 
 export default router;
